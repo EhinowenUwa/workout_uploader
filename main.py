1511b28a73e3961c3464aea3a57992f7edabb717
Astokchokxrselak <thedi3s323@gmail.com>
2026-04-11 23:50:53 -0400

    added colored printing to terminal, skeleton of cli loop

---
+++ main.py
@@ -0,0 +1,33 @@
+import cli
+from vars import *
+
+def cprint(col, *args, **kwargs):
+    print(col, end="")
+    print(*args, **kwargs)
+    print(RESET, end="")
+
+def success_print(*args, **kwargs):
+    cprint(GREEN, *args, **kwargs)
+
+def err_print(*args, **kwargs):
+    cprint(RED, *args, **kwargs)
+
+def def_print(*args, **kwargs):
+    cprint(RESET, *args, **kwargs)
+
+def out_print(*args, **kwargs):
+    cprint(WHITE, *args, **kwargs)
+
+def cli_iter():
+    def_print("Enter command: ", end="")
+    cmd = input()
+
+
+
+def cli_main():
+    success_print("This is the workout uploader CLI.")
+    while True:
+        cli_iter()
+
+if __name__ == "__main__":
+    cli_main()
