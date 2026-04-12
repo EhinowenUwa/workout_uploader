7f85bb4fbcb39af071e5e90adc37874801232c4f
Astokchokxrselak <thedi3s323@gmail.com>
2026-04-11 23:32:35 -0400

    created python file, environment variables parser

---
+++ main.py
@@ -0,0 +1,14 @@
+def _read_env(env):
+    body = {}
+    env = env.split("\n")
+    for var in env:
+        key, value = var.split("=")
+        body[key] = value
+    return body
+
+with open(".env", "r") as f:
+    env = _read_env(f.read())
+
+API_ENDPOINT = env["API_ENDPOINT"]
+API_CMD = "API_CMD" + "?cmd="
+print(API_CMD)
